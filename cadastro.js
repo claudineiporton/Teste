582972206ffc7d5ef39ed031cc2c51ff1646f108
Claudinei.Porto <claudineiporton@gmail.com>
2023-02-16 21:07:26 -0300

    Meu terceiro commit

---
+++ cadastro.js
@@ -0,0 +1,19 @@
+
+
+
+function pegaDados ()
+{
+
+
+let ites = document.getElementsByClassName('m');
+
+
+for (i= 0; i < ites.length; i++)
+{
+  
+  document.writeln(ites[0].value + ' - ' + ites[1].value + ' - ' + ites[2].value ) ;  
+
+}
+
+
+}
